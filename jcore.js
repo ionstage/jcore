9c50b631dbd423723d1e3fbb86631815227e8889
nasitra <nasitra@users.noreply.github.com>
2016-04-15 21:51:31 +0900

    Add module implementation

--- jcore.js
+++ jcore.js
@@ -1,7 +1,99 @@
 (function(global) {
   'use strict';
 
-  var jcore = {};
+  if (!global.requestAnimationFrame) {
+    global.requestAnimationFrame = function(callback) {
+      return setTimeout(callback, 1000 / 60);
+    };
+  }
+
+  var identity = function(value) {
+    return value;
+  };
+
+  var Component = function() {
+    this.relations = this.prop([]);
+  };
+
+  Component.prototype.prop = function(initialValue, defaultValue, converter) {
+    if (typeof converter !== 'function')
+      converter = identity;
+
+    var cache = converter(initialValue, defaultValue);
+
+    return function(value) {
+      if (typeof value === 'undefined')
+        return cache;
+
+      if (value === cache)
+        return;
+
+      cache = converter(value, cache);
+
+      this.markDirty();
+    };
+  };
+
+  Component.prototype.redraw = function() {};
+
+  Component.prototype.markDirty = (function() {
+    var dirtyComponents = [];
+    var requestId = null;
+
+    var updateRelations = function(index) {
+      for (var i = index, len = dirtyComponents.length; i < len; i++) {
+        var component = dirtyComponents[i];
+        component.relations().forEach(function(relation) {
+          relation.update(component);
+        });
+      }
+
+      // may be inserted other dirty components by updating relations
+      if (dirtyComponents.length > len)
+        updateRelations(len);
+    };
+
+    var callback = function() {
+      updateRelations(0);
+
+      dirtyComponents.forEach(function(component) {
+        component.redraw();
+      });
+
+      dirtyComponents = [];
+      requestId = null;
+    };
+
+    return function() {
+      if (dirtyComponents.indexOf(this) === -1)
+        dirtyComponents.push(this);
+
+      if (requestId !== null)
+        return;
+
+      requestId = global.requestAnimationFrame(callback);
+    };
+  })();
+
+  var Relation = function() {};
+
+  Relation.prototype.prop = function(initialValue) {
+    var cache = initialValue;
+
+    return function(value) {
+      if (typeof value === 'undefined')
+        return cache;
+
+      cache = value;
+    };
+  };
+
+  Relation.prototype.update = function() {};
+
+  var jcore = {
+    Component: Component,
+    Relation: Relation
+  };
 
   if (typeof module !== 'undefined' && module.exports)
     module.exports = jcore;
