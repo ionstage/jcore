79a838061742e5d39cb1693e9037a8f7a22fcbea
nasitra <nasitra@users.noreply.github.com>
2016-04-18 18:33:21 +0900

    Modify functions of Array for legacy browsers

--- jcore.js
+++ jcore.js
@@ -11,6 +11,15 @@
     return value;
   };
 
+  var lastIndexOf = function(array, value) {
+    for (var i = array.length - 1; i >= 0; i--) {
+      if (array[i] === value)
+        return i;
+    }
+
+    return -1;
+  };
+
   var Component = function() {
     this.relations = this.prop([]);
   };
@@ -41,31 +50,33 @@
     var requestId = null;
 
     var updateRelations = function(index) {
-      for (var i = index, len = dirtyComponents.length; i < len; i++) {
-        var component = dirtyComponents[i];
-        component.relations().forEach(function(relation) {
-          relation.update(component);
-        });
+      for (var ci = index, clen = dirtyComponents.length; ci < clen; ci++) {
+        var component = dirtyComponents[ci];
+        var relations = component.relations();
+
+        for (var ri = 0, rlen = relations.length; ri < rlen; ri++) {
+          relations[ri].update(component);
+        }
       }
 
       // may be inserted other dirty components by updating relations
-      if (dirtyComponents.length > len)
-        updateRelations(len);
+      if (dirtyComponents.length > clen)
+        updateRelations(clen);
     };
 
     var callback = function() {
       updateRelations(0);
 
-      dirtyComponents.forEach(function(component) {
-        component.redraw();
-      });
+      for (var i = 0, len = dirtyComponents.length; i < len; i++) {
+        dirtyComponents[i].redraw();
+      }
 
       dirtyComponents = [];
       requestId = null;
     };
 
     return function() {
-      if (dirtyComponents.indexOf(this) === -1)
+      if (lastIndexOf(dirtyComponents, this) === -1)
         dirtyComponents.push(this);
 
       if (requestId !== null)
