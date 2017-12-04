213ca8e347a668c5dfd0c0b5ae9f9fb13f086c81
nasitra <nasitra@users.noreply.github.com>
2017-12-04 19:21:45 +0900

    Remove property for superior constructor

--- jcore.js
+++ jcore.js
@@ -14,7 +14,6 @@
   }
 
   var inherits = function(ctor, superCtor) {
-    ctor.super_ = superCtor;
     ctor.prototype = Object.create(superCtor.prototype, {
       constructor: {
         value: ctor,
